3c5b9b562f02bd35fe9a9ad8d869576a54c2905f
Aakash Chahal <aakash.chahal62@gmail.com>
2023-11-29 23:07:00 +0000

    just fixing up the header

--- interface/good-app/src/App.js
+++ interface/good-app/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // Get theme from browser preference
+    const theme = window.matchMedia("(prefers-color-scheme: dark)").matches
+        ? "dark"
+        : "light";
+
+    return (
+        <div className={`App App-${theme}`}>
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <a className="App-link" href="/" rel="noopener noreferrer">
+                    Good App
+                </a>
+            </header>
+
+            <div className="App-body">
+                <p>
+                    <a href="/bad-app" rel="noopener noreferrer">
+                        Bad App
+                    </a>
+                </p>
+                <p>
+                    <a href="/good-app" rel="noopener noreferrer">
+                        Good App
+                    </a>
+                </p>
+            </div>
+        </div>
+    );
 }
 
 export default App;
