fe6bc10f25264fdbdc74e57c85db2e1c03dbd5b0
Aakash Chahal <aakash.chahal62@gmail.com>
2023-11-30 21:02:20 +0000

    done something with a theme toggle switch - still need to fix it

--- interface/good-app/src/App.js
+++ interface/good-app/src/App.js
@@ -1,11 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+import { useState } from "react";
+
 function App() {
     // Get theme from browser preference
-    const theme = window.matchMedia("(prefers-color-scheme: dark)").matches
-        ? "dark"
-        : "light";
+    const [theme, setTheme] = useState(
+        window.matchMedia("(prefers-color-scheme: dark)").matches
+            ? "dark"
+            : "light"
+    );
 
     return (
         <div className={`App App-${theme}`}>
@@ -14,16 +18,36 @@ function App() {
                 <a className="App-link" href="/" rel="noopener noreferrer">
                     Good App
                 </a>
+                <label className="App-switch">
+                    <input
+                        type="checkbox"
+                        checked={theme === "dark"}
+                        onChange={(e) => {
+                            setTheme(e.target.checked ? "dark" : "light");
+                        }}
+                    />
+                    <span className="App-slider round">
+                        {theme === "dark" ? "dark" : "light"}
+                    </span>
+                </label>
             </header>
 
             <div className="App-body">
                 <p>
-                    <a href="/bad-app" rel="noopener noreferrer">
+                    <a
+                        className="App-link"
+                        href="/bad-app"
+                        rel="noopener noreferrer"
+                    >
                         Bad App
                     </a>
                 </p>
                 <p>
-                    <a href="/good-app" rel="noopener noreferrer">
+                    <a
+                        className="App-link"
+                        href="/good-app"
+                        rel="noopener noreferrer"
+                    >
                         Good App
                     </a>
                 </p>
