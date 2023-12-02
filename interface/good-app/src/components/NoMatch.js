2cbc3060b8db1f7a66f859c49e2aac235083e9e3
Aakash Chahal <aakash.chahal62@gmail.com>
2023-12-02 21:25:33 +0000

    new font still need to fix overflow issue but thats fine

--- interface/good-app/src/components/NoMatch.js
+++ interface/good-app/src/components/NoMatch.js
@@ -1,9 +1,17 @@
 import React from "react";
 
-const NoMatch = () => {
+const NoMatch = (props) => {
+    console.log(props);
     return (
         <div>
-            <h2>404 Not Found</h2>
+            <h2
+                className="error-404"
+                style={{
+                    color: "red",
+                }}
+            >
+                404!
+            </h2>
             <p>The page you are looking for does not exist.</p>
         </div>
     );
